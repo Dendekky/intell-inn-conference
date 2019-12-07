3cbcc557ccf04582c941a72e5555ccc7e06547b3
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2019-12-07 13:21:23 +0100

    feat: create pages to consume the routes from our server

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import HeaderLinks from './components/Header/HeaderLinks';
 import AttendeeForm from './pages/attendform';
+import TalkForm from './pages/talkform';
+import AssignedTalkForm from './pages/assigntalkform'
 
-const App = () => <Router>
+export default function  App (props) {
+    const { ...rest } = props;
+    return(
+    <Router>
     <div>
-    <Route exact path='/' component={AttendeeForm}></Route>
+    <Header
+        absolute
+        color="transparent"
+        brand="The Handyman"
+        rightLinks={<HeaderLinks />}
+        {...rest}
+      />
+    <Route exact path='/' component={AttendeeForm} />
+    <Route path='/addtalk' component={TalkForm} />
+    <Route path='/talk/assign' component={AssignedTalkForm} />
     </div>
-    </Router>;
- 
-export default App;
+    </Router>
+    )
+}; 
